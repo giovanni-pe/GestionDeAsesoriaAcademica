020ee511607ba9f31a7a5c0302268b38315bfec7
Luz <cabiaadrianol@gmail.com>
2024-05-19 18:46:59 -0500

    crear repositorio: entity

--- CleanArchitecture.Domain/Entities/ResearchLine.cs
+++ CleanArchitecture.Domain/Entities/ResearchLine.cs
@@ -3,31 +3,30 @@ using System.Collections.Generic;
 
 namespace CleanArchitecture.Domain.Entities;
 public class ResearchLine : Entity
+{
+    public string Name { get; private set; }
+    public Guid ResearchGroupId { get; private set; }
+    public string Code { get; private set; }
+    public virtual ResearchGroup ResearchGroup { get; private set; } = null!;
+
+    public ResearchLine(Guid id, string name, Guid researchGroupId, string code) : base(id)
     {
-        public string Name { get; private set; }
-        public Guid ResearchGroupId { get; private set; }
-        public string Code { get; private set; }
-        public virtual ResearchGroup ResearchGroup { get; private set; } = null!;
-
-        public ResearchLine(Guid id, string name, Guid researchGroupId,string code) : base(id)
-        {
-         Name = name;
-         ResearchGroupId = researchGroupId;
+        Name = name;
+        ResearchGroupId = researchGroupId;
         Code = code;
-        }
-
-        public void SetName(string name)
-        {
-         Name = name;
-        }
-    // creado entidad 
-        public void SetResearchGroupId(Guid researchGroupId)
-        {
-            ResearchGroupId = researchGroupId;
-        }
-        public void SetCode(string code)
-        {
-            Code = code;
-        }
     }
 
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+    // creado entidad 
+    public void SetResearchGroupId(Guid researchGroupId)
+    {
+        ResearchGroupId = researchGroupId;
+    }
+    public void SetCode(string code)
+    {
+        Code = code;
+    }
+}
--- CleanArchitecture.Infrastructure/Migrations/20240508060813_AddResearchGroupAndLine.cs
+++ CleanArchitecture.Infrastructure/Migrations/20240508060813_AddResearchGroupAndLine.cs
@@ -25,18 +25,7 @@ namespace CleanArchitecture.Infrastructure.Migrations
                     table.PrimaryKey("PK_ResearchGroups", x => x.Id);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Tenants",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    Deleted = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Tenants", x => x.Id);
-                });
+            
 
             migrationBuilder.CreateTable(
                 name: "ResearchLines",
@@ -59,62 +48,26 @@ namespace CleanArchitecture.Infrastructure.Migrations
                         onDelete: ReferentialAction.Restrict);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(320)", maxLength: 320, nullable: false),
-                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Password = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
-                    Role = table.Column<int>(type: "int", nullable: false),
-                    Status = table.Column<int>(type: "int", nullable: false),
-                    LastLoggedinDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
-                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Deleted = table.Column<bool>(type: "bit", nullable: false)
-                },
-                /// <checked />
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Users_Tenants_TenantId",
-                        column: x => x.TenantId,
-                        principalTable: "Tenants",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
-
+        
+                
             migrationBuilder.InsertData(
                 table: "ResearchGroups",
                 columns: new[] { "Id", "Code", "Deleted", "Name" },
                 values: new object[] { new Guid("b542bf25-134c-47a2-a0df-84ed14d03c41"), "SW123", false, "INGENIERIA DE SOFTWARE" });
 
-            migrationBuilder.InsertData(
-                table: "Tenants",
-                columns: new[] { "Id", "Deleted", "Name" },
-                values: new object[] { new Guid("b542bf25-134c-47a2-a0df-84ed14d03c4a"), false, "Admin Tenant" });
-
+            
             migrationBuilder.InsertData(
                 table: "ResearchLines",
                 columns: new[] { "Id", "Code", "Deleted", "Name", "ResearchGroupId" },
                 values: new object[] { new Guid("b542bf25-134c-47a2-a0df-84ed14d03c42"), "ASW123", false, "Arquitectura de Software", new Guid("b542bf25-134c-47a2-a0df-84ed14d03c41") });
 
-            migrationBuilder.InsertData(
-                table: "Users",
-                columns: new[] { "Id", "Deleted", "Email", "FirstName", "LastLoggedinDate", "LastName", "Password", "Role", "Status", "TenantId" },
-                values: new object[] { new Guid("7e3892c0-9374-49fa-a3fd-53db637a40ae"), false, "admin@email.com", "Admin", null, "User", "$2a$12$Blal/uiFIJdYsCLTMUik/egLbfg3XhbnxBC6Sb5IKz2ZYhiU/MzL2", 0, 0, new Guid("b542bf25-134c-47a2-a0df-84ed14d03c4a") });
-
+           
             migrationBuilder.CreateIndex(
                 name: "IX_ResearchLines_ResearchGroupId",
                 table: "ResearchLines",
                 column: "ResearchGroupId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Users_TenantId",
-                table: "Users",
-                column: "TenantId");
+          
         }
 
         /// <inheritdoc />
